5a36579b9ba25c939e3a144f124dffd39a096c91
Justin <justinhobbs9108@gmail.com>
2019-09-05 06:10:46 -0400

    Updated Index and the javascripts

--- static/js/IndividualStats.js
+++ static/js/IndividualStats.js
@@ -1,11 +1,43 @@
-function unpack(rows, index) {
-    return rows.map(function(row){
-        return rows.map(row) {
-            return row[index];
-        }
-    });
+function handleSubmitStatCompare() {
+    d3.event.preventDefault();
+
+    var pokemonA = d3.select("#pokemonInputC").node().value;
+    console.log(pokemonA);
+    d3.select("#pokemonInputC").node().value = "";
+
+    var pokemonB = d3.select("#pokemonInputC").node().value;
+    console.log(pokemonB);
+    d3.select("#pokemonInputC").node().value = "";
+
+    buildStatComparePlot(pokemonC);
 }
 
-d3.json('/graphcount', function(data) {
-    
-})
+function buildStatComparePlot(pokemonC) {
+    var url = "/pokemon";
+    d3.json(url).then(function(datalist) {
+        console.log(datalist);
+        var PokemonC = datalist.filter(name = pokemonC);
+        var nameC = PokemonC.Name;
+        var TotalC = PokemonC.Total;
+        var HPC = PokemonC.HP;
+        var AttackC = PokemonC.Attack;
+        var DefenseC = PokemonC.Defense;
+        var SpAtkC = PokemonC.SpAtk;
+        var SpDefC = PokemonC.SpDef;
+        var SpeedC = PokemonC.Speed;
+        var PokemonStatsC = [nameC, TotalC ,HPC ,AttackC ,,DefenseC ,SpAtkC ,SpDefC ,SpeedC ];
+
+        var trace = {
+            type: "bar",
+            name: name,
+            x: PokemonStatsC,
+            y: Range(auntomap)
+        };
+        var data = [trace];
+        var layout = { 
+            title: "Stat Compare",
+            barmode: "group"
+        };
+        Plotly.newPlot("IndividualStats", data, layout);
+    });
+}
--- static/js/StatCompare.js
+++ static/js/StatCompare.js
@@ -1,10 +1,60 @@
-function unpack(rows, index) {
-    return rows.map(function(row){
-        return rows.map(row) {
-            return row[index];
-        }
-    });
+function handleSubmitStatCompare() {
+    d3.event.preventDefault();
+
+    var pokemonA = d3.select("#pokemonInputA").node().value;
+    console.log(pokemonA);
+    d3.select("#pokemonInputA").node().value = "";
+
+    var pokemonB = d3.select("#pokemonInputB").node().value;
+    console.log(pokemonB);
+    d3.select("#pokemonInputB").node().value = "";
+
+    buildStatComparePlot(pokemonA,pokemonB);
 }
 
-function build
+function buildStatComparePlot(pokemonA,pokemonB) {
+    var url = "/pokemon";
+    d3.json(url).then(function(datalist) {
+        console.log(datalist);
+        var PokemonA = datalist.filter(name = pokemonA);
+        var nameA = PokemonA.Name;
+        var TotalA = PokemonA.Total;
+        var HPA = PokemonA.HP;
+        var AttackA = PokemonA.Attack;
+        var DefenseA = PokemonA.Defense;
+        var SpAtkA = PokemonA.SpAtk;
+        var SpDefA = PokemonA.SpDef;
+        var SpeedA = PokemonA.Speed;
+        var PokemonStatsA = [nameA, TotalA,HPA,AttackA,,DefenseA,SpAtkA,SpDefA,SpeedA];
 
+        var trace1 = {
+            type: "bar",
+            name: name,
+            x: automap,
+            y: PokemonStatsA,
+        };
+        var PokemonB = datalist.filter(name = PokemonB);
+        var nameB = pokemonB.Name;
+        var TotalB = PokemonB.Total;
+        var HPB = PokemonB.HP;
+        var AttackB = PokemonB.Attack;
+        var DefenseB = PokemonB.Defense;
+        var SpAtkB= PokemonB.SpAtk;
+        var SpDefB = PokemonB.SpDef;
+        var SpeedB = PokemonB.Speed;
+        var PokemonStatsB = [nameB, TotalB,HPB,AttackB,,DefenseB,SpAtkB,SpDefB,SpeedB];
+
+        var trace2 = {
+            type: "bar",
+            name: name,
+            x:automap,
+            y: PokemonStatsB,
+        }
+        var data = [trace1, trace2];
+        var layout = { 
+            title: "Stat Compare",
+            barmode: "group"
+        };
+        Plotly.newPlot("statComparePlot", data, layout);
+    });
+}
